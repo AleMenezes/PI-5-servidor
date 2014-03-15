0489d90820e8597e71c55ff90d1f8e90e7be4a48
Alessandro Camillo Gimenez de Menezes <alessandrocamillogimenezdemenezes@Alessandro-Mack.local>
2014-03-14 21:02:29 -0300

    Initial Commit

---
+++ servidor/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  servidor
+//
+//  Created by Alessandro Camillo Gimenez de Menezes on 14/03/14.
+//  Copyright (c) 2014 Alessandro Camillo Gimenez de Menezes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
